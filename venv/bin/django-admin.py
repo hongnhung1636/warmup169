66c5e9812833ed4ae24022619ff87a9d5f8aefb9
hong <hongnhung1636@gmail.com>
2013-02-14 01:19:47 -0800

    warmup project cs169

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hongle/Documents/workspace/warmupprojectcs169/src/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
