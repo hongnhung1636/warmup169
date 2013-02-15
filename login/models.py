28c955af48d96c0a1b0d8e0aaf43321433f7e652
hong <hongnhung1636@gmail.com>
2013-02-15 04:21:05 -0800

    try111

--- login/models.py
+++ login/models.py
@@ -4,7 +4,7 @@ from django.db import models
 SUCCESS               =   1  # : a success
 ERR_BAD_CREDENTIALS   =  -1  # : (for login only) cannot find the user/password pair in the database
 ERR_USER_EXISTS       =  -2  # : (for add only) trying to add a user that already exists
-ERR_BAD_USERNAME      =  -3  # : (for add, or login) invalid user name (only empty string is invalid for now)
+ERR_BAD_USERNAME      =  -3  
 ERR_BAD_PASSWORD      =  -4
 
 max_length = 128
